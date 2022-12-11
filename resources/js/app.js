48989d7b756d87fcc8496da257d7eb0f879a67ec
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-11 20:13:39 +0100

    comienzo del diseño del area publica del sistema. El uso del logo y las clases

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
-
+import '../css/app.css';
 import Alpine from 'alpinejs';
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+  ]);
 
 window.Alpine = Alpine;
 
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,26 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Proyecto web</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto web</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
+
 <body>
-	<p>
-		<a href="{{ route('home') }}">Home</a>
-		<a href="{{ route('blog') }}">Blog</a>
-        @auth
-        <a href="{{ route ('dashboard') }}">Dashboard</a>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                <img src="{{ Vite::asset('resources/images/logo.png') }}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" name="" id="" placeholder="Buscar">
+                </form>
 
-        @else
-        <a href="{{ route ('login') }}">login</a>
+            </div>
+            @auth
+            <a href="{{ route ('dashboard') }}">Login</a>
 
-        @endauth
-	</p>
+            @else
+            <a href="{{ route ('login') }}">login</a>
 
-	<hr>
+            @endauth
+        </header>
 
-	@yield('content')
+
+        @yield('content')
+    </div>
+
+
 </body>
+
 </html>
